9f8268e735ca4826b87f885926c9458e0672be84
faceyogavn <housenest0@gmail.com>
2026-03-11 10:18:15 +0700

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose.connect(process.env.MONGO_URI,
                     useUnifiedTopology: true
                   })
   .then(() => {    
-    console.log("MongoDB connected ✅"));
+    console.log("MongoDB connected ✅");
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
@@ -204,6 +204,7 @@ app.post("/api/admin/refresh", (req, res) => {
 
 
 //console.log(Order.schema.obj);
+
 
 
 
